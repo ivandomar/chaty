88016f0fc303d7410842da8a4808038c415f9b05
Ivan Cézanne <ivancezanne.hrzon@gmail.com>
2020-04-01 22:18:53 -0300

    refactor(index): implements main module under ES6
    add interactions
    
    caused jitsi api to not get media devices

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-$(document).ready(() => {
-  const domain = 'meet.jit.si';
-  const options = {
-      roomName: 'JitsiMeetAPIExample',
-      width: 700,
-      height: 700,
-      parentNode: $('#meet')[0]
-  };
+import call from './src/call';
 
-  const api = new JitsiMeetExternalAPI(domain, options);
+$(document).ready(() => {
+
+  let myCall = null;
+
+  $('button#startCall').click(() => {
+
+    const frame = $('#frame')[0];
+
+    call.setFrame(frame);
+
+    myCall = call.start();
+
+  });
+
+  $('button#seeCall').click(() => {
+
+    console.log(myCall);
+
+  });
+
 });
